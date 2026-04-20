6ad7be7e6c7c1e51c55fec3146be746e31a76477
FernandoRibeiro <1060064@isep.ipp.pt>
2026-04-20 12:32:23 +0100

    ui: use designer's official adidas SVG logo

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -65,7 +65,7 @@ function ChatbotIcon() {
 function AdidasLogo({ className }: { className?: string }) {
   return (
     <img
-      src="/adidas-logo.png"
+      src="/adidas-logo.svg"
       alt="adidas"
       className={className}
       draggable={false}
